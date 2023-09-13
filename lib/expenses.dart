2a0910481d701cf324ee654308c6609095861d94
mjr2595 <michaeljohnraymond@gmail.com>
2023-09-12 22:35:29 -0500

    first commit

---
+++ lib/expenses.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
+
+class _ExpenseState extends State<Expenses> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
