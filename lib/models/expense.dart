83f15ba40a150fd7192b61dccf3a0d7d84f8da87
mjr2595 <michaeljohnraymond@gmail.com>
2023-09-17 13:50:48 -0500

    Added: ExpenseItem and category icons

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,8 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:uuid/uuid.dart';
+
+final formatter = DateFormat.yMd();
 
 const uuid = Uuid();
 
 enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.fastfood,
+  Category.travel: Icons.flight,
+  Category.leisure: Icons.beach_access,
+  Category.work: Icons.work,
+};
 
 class Expense {
   final String id;
@@ -17,4 +28,8 @@ class Expense {
     required this.date,
     required this.category,
   }) : id = uuid.v4();
+
+  String get farmattedDate {
+    return formatter.format(date);
+  }
 }
